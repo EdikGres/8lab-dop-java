69ddc802f56f88c3957eefed2b4267f8eaf9eca0
Eduard_Churikov <edik_gres@icloud.com>
2022-12-14 05:54:10 +0300

    fix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,7 @@ public class Main {
 
     static int n, k;
     static int[] a;
+    static int arr[] = {1, 2, 3, 4};
 
     public static void main(String[] args) {
 
@@ -19,7 +20,7 @@ public class Main {
 //        r.run();
 //        a.run();
 //        b.run();
-        Generator generator = new Generator(1, 8, 10, 8);
+        Generator generator = new Generator(1, 4, 4, 8);
         System.out.println("multithreading: ");
         Date start = new Date();
         generator.generate();
@@ -28,19 +29,21 @@ public class Main {
 //        System.out.println("Time taken in milli seconds: " + );
 
         System.out.println("1 thread: ");
+
+        int n = arr.length;
+        int r = 4;
         start = new Date();
-        n = 8;
-        k = 10;
-        a = new int[k];
-        p2(0, 1);
+        CombinationRepetition(arr, n, r);
         end = new Date();
-//        System.out.println("Time taken in milli seconds: " + (end.getTime() - start.getTime()));
-        long one_thread = end.getTime() - start.getTime();
+        long one_thread2 = end.getTime() - start.getTime();
 
         System.out.println("MULTI: Time taken in milli seconds: " + multi);
-        System.out.println("1Thread: Time taken in milli seconds: " + one_thread);
+        System.out.println("1Thread: Time taken in milli seconds: " + one_thread2);
+
 
     }
+
+
     static void p2(int pos, int maxUsed) {
         if(pos == k) {
             System.out.println(Arrays.toString(a));
@@ -50,6 +53,40 @@ public class Main {
                 p2(pos+1,i);
             }
         }
+    }
+
+    static void CombinationRepetitionUtil(int chosen[], int arr[],
+                                          int index, int r, int start, int end) {
+        // Since index has become r, current combination is
+        // ready to be printed, print
+        if (index == r) {
+            for (int i = 0; i < r; i++) {
+                System.out.printf("%d ", arr[chosen[i]]);
+            }
+            System.out.printf("\n");
+            return;
+        }
+
+        // One by one choose all elements (without considering
+        // the fact whether element is already chosen or not)
+        // and recur
+        for (int i = start; i <= end; i++) {
+            chosen[index] = i;
+            CombinationRepetitionUtil(chosen, arr, index + 1,
+                    r, i, end);
+        }
+        return;
+    }
+
+    // The main function that prints all combinations of size r
+// in arr[] of size n with repetitions. This function mainly
+// uses CombinationRepetitionUtil()
+    static void CombinationRepetition(int arr[], int n, int r) {
+        // Allocate memory
+        int chosen[] = new int[r + 1];
+
+        // Call the recursive function
+        CombinationRepetitionUtil(chosen, arr, 0, r, 0, n - 1);
     }
 
 }
